571ec2ef7296d49edb1e25261c139730f51ae6af
Dina Spencer <dkay22@gmail.com>
2022-09-22 12:20:31 +0200

    Bring back jwt authentication

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/users', (req, res) => {
 
 //READ GET all movies
 app.get('/movies', 
-  //passport.authenticate('jwt', {session: false}), 
+  passport.authenticate('jwt', {session: false}), 
   (req, res) => {
   Movies.find()
   .then((movies) => {
